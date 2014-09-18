121bb8ad674dee91f0126a1dbe4baa23627d1cf8
jzdev <jzdev@outlook.com>
2014-09-17 23:44:35 -0400

    Initial add of P1 project - Dynamic Web Apps Portfolio

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Carolyne Towner - P1 - Dynamic Web Apps Portfolio</title>
+	<meta charset="utf-8">
+	<link href="css/main.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id="mainContent">
+		<h1>Carolyne Towner<br>Dynamic Web Apps Portfolio</h1>
+		<img src="img/dog.png" alt="Carolyne Towners Dog">
+		<ul class="projects">
+			<li><h3>Project 2 Title</h3>
+				<ul>
+					<li><a href="http://p2.jztech.me/">View</a></li>
+					<li><a href="https://github.com/jzhes/p2">Github</a></li>
+				</ul>
+			</li>
+
+			<li><h3>Project 3 Title</h3>
+				<ul>
+					<li><a href="http://p3.jztech.me/">View</a></li>
+					<li><a href="https://github.com/jzhes/p3">Github</a></li>
+				</ul>
+			</li>
+
+			<li><h3>Project 4 Title</h3>
+				<ul>
+					<li><a href="http://p4.jztech.me/">View</a></li>
+					<li><a href="https://github.com/jzhes/p4">Github</a></li>
+				</ul>
+			</li>
+		</ul>
+		
+		<h2>In 2+ sentences describe who you are and what you do.</h2>
+		<p>I am currently a stay-at-home mom. I have two teenage sons in addition to the dog above who keep me very busy!</p>
+		
+		<h2>Why are you taking this course?</h2>
+		<p>I am taking this course because I need to go back to work and desperately(!) need to update my very "stale" skill set.</p>
+		
+		<h2>What previous experience do you have with programming?</h2>
+		<p>I have some programming experience from quite some time ago before I left work to be home with my children.</p>
+		
+		<h2>Are you working on a Mac, PC, or some other platform?</h2>
+		<p>I am working on a PC running Windows 7.</p>
+		
+		<h2>Anything else we should know?</h2>
+		<p>I am enjoying learning about all of the new technologies and am very excited to get back into the workforce!</p>
+	</div>
+</body>
+</html>
